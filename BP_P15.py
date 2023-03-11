54f54d3ef7e2ca83c49f395958fc32b602b21cf9
go2collage <91822422+go2collage@users.noreply.github.com>
2023-03-11 22:41:07 +0530

    Add files via upload

---
+++ BP_P15.py
@@ -0,0 +1,13 @@
+# Basic Python Program
+
+# print even number between 1 to 100
+
+num = int(input("Enter even number range between 1 to ....: "))
+i = 1       
+
+while i <= num:
+    if i % 2 == 0:
+        print(i)
+    i = i + 1
+
+    # You can also print 2's table
